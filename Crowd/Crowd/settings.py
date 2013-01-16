69c90ddc5e7f3949828f87ba5f0be078aa89391b
giginet <giginet.net@gmail.com>
2013-01-16 15:10:27 +0900

    開発用サーバーをfixtureに追加

--- Crowd/Crowd/settings.py
+++ Crowd/Crowd/settings.py
@@ -66,6 +66,10 @@ STATIC_ROOT = '/static/'
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
+FIXTURE_DIRS = {
+        os.path.join(ROOT, 'Crowd/fixtures')
+}
+
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
